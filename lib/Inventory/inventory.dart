2518c63fe95ce25bfc1e77a94175b4dfb5e59e3a
Shameen Shetty <shettyshameen1@gmail.com>
2022-07-15 16:14:17 -0500

    created inventory class added fonts added 'add to inventory' icon

---
+++ lib/Inventory/inventory.dart
@@ -0,0 +1,11 @@
+import 'dart:html';
+
+class Inventory {
+  String itemName = '';
+  String itemPicture = '';
+
+  Inventory({itemName, itemPicture}) {
+    this.itemName = itemName;
+    this.itemPicture = itemPicture;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,65 @@ class Inventory extends StatefulWidget {
 }
 
 class _InventoryState extends State<Inventory> {
+  List<String> currentInventory = [
+    'garam masala',
+    'chole masala',
+    'red chilli powder',
+    'turmeric powder',
+    'kheer powder',
+    'potato chips',
+    'tortillas',
+    'besan flour',
+    'rajma'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [],
+      appBar: AppBar(
+        title: const Text(
+          'Inventory Management App',
+          style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Arima'),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue[400],
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Text('First Text'),
+              Text('Second Text'),
+              Text('Third Text'),
+              ElevatedButton(
+                onPressed: () {
+                  print("you clicked on the elevated button");
+                },
+                child: Icon(Icons.mail),
+              ),
+
+              // column of quotes, mapping quotes to a list of text widgets
+              Column(
+                  children: currentInventory
+                      .map((inventoryItem) => Text(inventoryItem))
+                      .toList()),
+
+              // icon button
+              IconButton(
+                  iconSize: 50.0,
+                  onPressed: () {
+                    print("you pressed the icon button\n");
+                  },
+                  icon: const Icon(Icons.addchart_rounded))
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Image.asset('assets/images/add_item_to_inventory.png'),
+        onPressed: () {
+          print('adding a new item to the inventory');
+        },
       ),
     );
   }
