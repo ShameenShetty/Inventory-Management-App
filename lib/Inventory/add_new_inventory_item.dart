ebb16acc8e9af36e140f9dacb4e948e59cca0fc8
Shameen Shetty <shettyshameen1@gmail.com>
2022-07-20 14:55:35 -0500

    updated the 'add new inventory item' page to include textfield for the items picture link, and items name or description
    added new btn "Add Item"

--- lib/Inventory/add_new_inventory_item.dart
+++ lib/Inventory/add_new_inventory_item.dart
@@ -1,18 +1,91 @@
 import 'package:flutter/material.dart';
 
 class AddNewInventoryItem {
+  String itemPicture = '';
+  String itemName = '';
+
   Widget addNewItemPage(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Second Route'),
+        title: Text('Add New Item to Inventory',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Arima',
+              fontSize: 20,
+              color: Colors.purple[800],
+            )),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+        child: Column(
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              color: Colors.amber.shade50,
+              clipBehavior: Clip.antiAlias,
+              margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+              child: Column(children: [
+                // Adding item picture link
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextField(
+                    onChanged: (text) {
+                      print('text field item picture: $text');
+                      itemPicture = text;
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Add the item\'s picture',
+                    ),
+                  ),
+                ),
+
+                // Adding item description or name
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextField(
+                    onChanged: (text) {
+                      print('text field item description: $text');
+                      itemName = text;
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Add the item name or description',
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate back to first route when tapped.
+                    print(
+                        'adding the item to the inventory, name is $itemName, pic link is $itemPicture');
+                  },
+                  child: const Text('Add Item'),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate back to first route when tapped.
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Exit'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
